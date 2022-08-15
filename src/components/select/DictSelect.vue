71299a8529ebe0b386e4875abd4cf0bddc8dc66c
hailong <haotian.chen@tuya.com>
2022-08-15 20:27:43 +0800

    help

---
+++ src/components/select/DictSelect.vue
@@ -0,0 +1,22 @@
+<template>
+  <a-select placeholder="DictSelect 数据" style="width: 200px">
+    <a-select-option v-for="item in dicts.value2" :value="item" :key="item">{{
+      item
+    }}</a-select-option>
+  </a-select>
+</template>
+<script>
+export default {
+  name: "DictSelect",
+  data() {
+    return {};
+  },
+  computed: {
+    dicts() {
+      const value = this.$store.getters["account/dicts"];
+      console.log("DictSelect value", value);
+      return value;
+    },
+  },
+};
+</script>
